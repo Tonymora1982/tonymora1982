6fe1f36ec34e4c3f62c22d55ce7ac05130ed312a
Anthony Mora P <tonymora1982@gmail.com>
2023-05-21 23:02:29 -0600

    Github page

---
+++ script.js
@@ -0,0 +1,14 @@
+const themeSwitchToggle = document.getElementById('theme-switch-toggle');
+
+themeSwitchToggle.addEventListener('change', () => {
+    document.body.classList.toggle('dark-mode');
+});
+
+
+
+
+
+
+
+
+
