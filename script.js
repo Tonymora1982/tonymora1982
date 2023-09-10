becfd250c334265e1730d2470af9ee3a951d747c
Anthony Mora P <tonymora1982@gmail.com>
2023-09-10 11:29:20 -0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,19 @@ themeSwitchToggle.addEventListener('change', () => {
     document.body.classList.toggle('dark-mode');
 });
 
+// Ejemplo: Código para cambiar el estilo de un enlace al pasar el ratón por encima
+document.addEventListener("DOMContentLoaded", function() {
+    const sidebarLinks = document.querySelectorAll("#sidebar a");
+
+    sidebarLinks.forEach(link => {
+        link.addEventListener("mouseover", function() {
+            this.style.color = "#007BFF";  // Cambiar el color al azul
+        });
+        link.addEventListener("mouseout", function() {
+            this.style.color = "";  // Restablecer el color
+        });
+    });
+});
 
 
 
